ce699b7fe817f44a59c9b6e57b544f02215ef7e4
Troels Nielsen <bn.troels@gmail.com>
2021-05-29 18:11:53 +0200

    Firebase deployment and functions

--- backend/init.js
+++ backend/init.js
@@ -2,7 +2,7 @@ const ChessWebAPI = require('chess-web-api');
 const cheerio = require('cheerio');
 const axios = require('axios');
 
-const chessAPI = new ChessWebAPI();
+const chessAPI = new ChessWebAPI({queue: true});
 
 async function getPlayer(playerName)  {
   return await chessAPI.getPlayer(playerName);
@@ -20,36 +20,94 @@ async function getPlayerTournaments(playerName) {
 
 async function printTournament(tournamentId) {
   try { 
-    const tournament = await chessAPI.getTournament(tournamentId);
-    console.log(tournament.body);
   } catch (e) {
     return null;
   }
 
 }
 
-async function getTournaments() {
+async function lookupTournament(tournamentId) {
+  const tournament = await chessAPI.getTournament(tournamentId);
+  return tournament.body;
+}
+
+async function lookupTournamentRounds(tournamentId, round) {
+  const tournamentRounds = await chessAPI.getTournamentRoundGroup(tournamentId, round);
+  return tournamentRounds.body;
+}
+
+async function* getNewTournaments() {
   const resp = await axios.get("https://www.chess.com/tournament/live/arena?&page=1");
   const $ = cheerio.load(resp.data);
   const links = $("a.tournaments-live-name");
-  const res = [];
   for (var i = 0; i < links.length; i++) {
     const link = links[i];
     const href = link.attribs["href"];
     const lastSlash = href.lastIndexOf("/");
     
-    res.push(href.slice(lastSlash + 1));
+    yield href.slice(lastSlash + 1);
   }
-  return res;
 }
 
-const tournaments = getTournaments();
-tournaments.then(x => {
-  x.forEach(printTournament)
-});
+async function* getUpcomingTournaments() {
+  const resp = await axios.get("https://www.chess.com/tournament/upcoming/arena?&page=1");
+  const $ = cheerio.load(resp.data);
+  const rows = $("tr")
+  for (var i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    let tdCount = 0;
+    let id, name, date;
+    for (var j = 0; j < row.children.length; j++) {
+      const child = row.children[j];
+      if (child.type !== 'tag' || child.name != 'td') {
+        continue;
+      }
+      if (tdCount === 0) {
+        id = $(child).text().trim();
+        console.log(id);
+      } else if (tdCount === 1) {
+        name = $(child).text().trim();
+        console.log(name);
+      } else if (tdCount === 5) {
+        const dateText = $(child).text().trim().replaceAll(/\s+/g, ' ');
+        date = new Date(new Date(dateText).toISOString().replace(/Z$/, "-08:00"));
+        console.log(date);
+      }
+      tdCount++;
+    }
+    if (name != null && id != null && date != null) {
+      yield {
+        name,
+        id,
+        date
+      }
+    }
+  }
+}
+
+function getTournamentId(tournamentObj) {
+  console.log(tournamentObj);
+  return `${tournamentObj.name.replaceAll(/\s/g, "-").replaceAll(/[^\w\d_-]/g, "")}-${tournamentObj.id}`;
+}
+
+async function getTournaments() {
+  for await(const value of getUpcomingTournaments()) {
+    console.log(value);
+    const tournamentId = getTournamentId(value);
+    const res = await lookupTournament(tournamentId);
+    console.log(res);
+    try {
+      const t = await lookupTournamentRounds(value, 1);
+      console.log(t);
+      return;
+    } catch (e) {}
     
-  //getPlayerTournaments("tniel");
-      //printTournament("10-bullet-1186180");
+  }
+}
+
+//lookupTournament('10-bullet-1186229').then(console.log)
+//lookupTournamentRounds('10-bullet-1186229', 1).then(console.log)
+//printTournament("10-bullet-1186180");
 
 
 
